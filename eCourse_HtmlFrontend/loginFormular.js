03b26f22045aa254977508ff2db2dcccbcfb07d9
Jan Hussmann <jan@familie-hussmann.de>
2021-03-30 15:31:14 +0200

    theoritsche Oberfläche für ein Loginformular

---
+++ eCourse_HtmlFrontend/loginFormular.js
@@ -0,0 +1,13 @@
+let formular = '        <h1>Login</h1>\n' +
+    '                        <formular>\n' +
+    '                          <label for="username">\n' +
+    '                              <i class="' +/*add label*/ +'"></i>\n' +
+    '                          </label>\n' +
+    '                          <input type="text" name="username" placeholder="Username" id="username" required>\n' +
+    '                          <label for="password">\n' +
+    '                              <i class="' +/*add label*/ +'"></i>\n' +
+    '                          </label>\n' +
+    '                          <input type="password" name="password" placeholder="Password" id="password" required>\n' +
+    '                          <button onclick="' +/*send data from fields to backend*/ +'">Login</button>\n' +
+    '                      </formular>';
+
