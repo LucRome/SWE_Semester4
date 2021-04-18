a58c04f53e49c8b21415f59d455678f46db7ece2
43-50ph14 <so_s@web.de>
2021-04-18 19:32:29 +0200

    only accepting pdfs
    *users can only upload pdf files
    *otherwise error

--- eCourse_backend/file_exchange/views.py
+++ eCourse_backend/file_exchange/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, FileResponse
+from django.http import HttpResponseRedirect, FileResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
 from .forms import FileForm
 from .models import Submission
+import magic
+import os
 
 # Create your views here.
 @login_required
@@ -20,6 +22,9 @@ def upload_file(request):
 @login_required
 def download_file(request, id):
     file = get_object_or_404(Submission, pk=id)
-    fn = file.file.path
-    response = FileResponse(open(fn, 'rb'))
+    file_buffer = open(file.file.path, 'rb').read()
+    content_type = magic.from_buffer(file_buffer, mime=True)
+    response = HttpResponse(file_buffer, content_type=content_type)
+    response['Content-Disposition'] = 'attachment; filename="%s' %os.path.basename(file.file.path)
     return response
+
