880174c7cd3a2d0cb28d32d82166955e348e3786
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-21 21:25:44 +0200

    useradministration: add student list + add username filter

--- eCourse_backend/users/forms.py
+++ eCourse_backend/users/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Form, ChoiceField, CharField
+from django.forms import ModelForm, Form, ChoiceField, CharField, IntegerField
 from users.models import User, Student, Lecturer, Office
 
 
@@ -11,5 +11,13 @@ class UserForm(ModelForm):
 # useradministration: filter forms
 
 class LecturerFilterForm(Form):
+    username = CharField(max_length=50, label="Username", required=False)
+    first_name = CharField(max_length=50, label="Vorname", required=False)
+    last_name = CharField(max_length=50, label="Nachname", required=False)
+
+
+class StudentFilterForm(Form):
+    matr_nr = IntegerField(label="Matrikel Nummer", required=False)
+    username = CharField(max_length=50, label="Username", required=False)
     first_name = CharField(max_length=50, label="Vorname", required=False)
     last_name = CharField(max_length=50, label="Nachname", required=False)
