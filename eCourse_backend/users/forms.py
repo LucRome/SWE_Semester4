9c4d07cefcdb9853fb3d6cdfe257e43176ba8f04
LucRome <64947931+LucRome@users.noreply.github.com>
2021-04-21 21:35:06 +0200

    lecturer list: add filter option (first_name + last_name)

--- eCourse_backend/users/forms.py
+++ eCourse_backend/users/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Form, ChoiceField
+from django.forms import ModelForm, Form, ChoiceField, CharField
 from users.models import User, Student, Lecturer, Office
 
 
@@ -6,3 +6,10 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'matr_nr']
+
+
+# useradministration: filter forms
+
+class LecturerFilterForm(Form):
+    first_name = CharField(max_length=50, label="Vorname", required=False)
+    last_name = CharField(max_length=50, label="Nachname", required=False)
