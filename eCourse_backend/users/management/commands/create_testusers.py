9b76aaf33e625697d110c15dfc714f3f0274e691
SWE-eCourseBot <SWE-eCourseBot@users.noreply.github.com>
2021-04-21 15:18:29 +0000

    Automated autopep8 fixes

--- eCourse_backend/users/management/commands/create_testusers.py
+++ eCourse_backend/users/management/commands/create_testusers.py
@@ -1,11 +1,14 @@
-import random, string
+import random
+import string
 from django.core.management.base import BaseCommand, CommandError
 from users.models import User
 
 USER_NUM = range(20)
 
+
 def rand_word(l):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(l))
+
 
 class Command(BaseCommand):
     help = 'Create 20 random users that can be used to test the application'
@@ -14,16 +17,17 @@ class Command(BaseCommand):
         for i in USER_NUM:
             t = (i % 3) + 1
             u = User(
-                    username=rand_word(10),
-                    first_name=rand_word(9),
-                    last_name=rand_word(10),
-                    email=rand_word(10) + '@example.com',
-                    matr_nr=random.randint(10000,9999999),
-                    type=t,
-                    )
+                username=rand_word(10),
+                first_name=rand_word(9),
+                last_name=rand_word(10),
+                email=rand_word(10) + '@example.com',
+                matr_nr=random.randint(10000, 9999999),
+                type=t,
+            )
             try:
                 u.save()
             except Exception:
                 raise CommandError('Could not create user')
 
-            self.stdout.write(self.style.SUCCESS('Successfully created testusers'))
+            self.stdout.write(self.style.SUCCESS(
+                'Successfully created testusers'))
