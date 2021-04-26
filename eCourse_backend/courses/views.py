cf8fcfd8b30c0007324baeaaaaeda3c6e867e394
43-50ph14 <so_s@web.de>
2021-04-26 14:55:50 +0200

    student, lecturer names instead of id
    *at the details view of a cours we now see the first and last name of all course members
    *for viewing I use the template courses/detail.html

--- eCourse_backend/courses/views.py
+++ eCourse_backend/courses/views.py
@@ -3,16 +3,37 @@ from django.forms.models import model_to_dict
 from django.contrib.auth.decorators import permission_required, login_required
 from .forms import CourseForm
 from .models import Course
+from users.models import User
+from django.db.models import Q
 
 # Create your views here.
 
 
 @login_required
-@permission_required('courses.create_course', raise_exception=True)
-def overview(request):
+def course_overview(request):
+    user_id = request.user.id
     if request.method == 'GET':
-        courses = Course.objects.all()
+        # officer has type 3 in db
+        if request.user.type == 3:
+            courses = Course.objects.all()
+        else:
+            # courses = Course.objects.filter(student = user_id)
+            courses = Course.objects.filter(Q(student = user_id) | Q(lecturer_id = user_id))
         return render(request, 'courses/overview.html', {'courses': courses})
+
+@login_required
+@permission_required('courses.create_course', raise_exception=True)
+def view_course(request, id):
+    if request.method == 'GET':
+        course = get_object_or_404(Course, pk = id)
+        lecturer_id = course.lecturer_id
+        lecturer = User.objects.get(id = lecturer_id)
+        lecturer_name = lecturer.first_name + ' ' + lecturer.last_name
+        students = list()
+        for student in course.student.all():
+            student_name = student.first_name + ' ' + student.last_name
+            students.append(student_name)
+    return render(request, 'courses/detail.html', {'lecturer': lecturer_name, 'students': students})
 
 
 @login_required
