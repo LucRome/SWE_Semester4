2869427dfa017058295b8f7773dd77b02c370dd2
LucRome <64947931+LucRome@users.noreply.github.com>
2021-05-08 11:28:26 +0200

    Fix edit course view
    - now courses are edited when save is hit (previously a new course was created)
    - add success msg
    - fix formatting

--- eCourse_backend/courses/views.py
+++ eCourse_backend/courses/views.py
@@ -151,10 +151,13 @@ def delete_course(request, id):
 @login_required
 @permission_required('course.change_course', raise_exception=True)
 def edit_course(request, id):
+    updata_success = False
     if request.method == 'POST':
-        form = CourseForm(request.POST)
+        course_object = get_object_or_404(Course, pk=id)
+        form = CourseForm(request.POST or None, instance=course_object)
         if form.is_valid():
             form.save()
+            updata_success = True
     else:
         course_object = get_object_or_404(Course, pk=id)
         form = CourseForm(model_to_dict(course_object))
@@ -164,5 +167,10 @@ def edit_course(request, id):
     elif request.user.type == 2:
         base_template = 'lecturer/home_lecturer.html'
 
-    return render(request, 'courses/edit_course.html',
-                  {'form': form, 'courseid': id, 'base_template': base_template})
+    context = {
+        'form': form, 
+        'courseid': id, 
+        'base_template': base_template,
+        'update_success': updata_success}
+
+    return render(request, 'courses/edit_course.html', context)
