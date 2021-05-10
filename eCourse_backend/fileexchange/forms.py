865640943551324464fd11d0926c92833b095c64
43-50ph14 <so_s@web.de>
2021-05-10 11:43:16 +0200

    fixed splitting date and time

--- eCourse_backend/fileexchange/forms.py
+++ eCourse_backend/fileexchange/forms.py
@@ -1,50 +1,51 @@
+from datetime import datetime
 from django import forms
-from django.forms.widgets import TextInput
-from django.forms import MultiWidget
+from django.forms import TextInput, MultiWidget, DateTimeField
 from django.contrib.admin import widgets
 from django.utils.dateparse import parse_duration
+from django.utils.timezone import make_aware
 from users.models import Lecturer
 from .models import Submission
 from courses.models import Exercise
 
-# DatetTime input fields
-# spliting datetimefiled into a date and a time filed (idk maybe frontend
-# needs to fit into their style)
 
+# source: https://gist.github.com/andytwoods/76f18f5ddeba9192d51dccc922086e43?fbclid=IwAR1x4GbeHLMQypWKaYPGn55r92-uCmJZqLUf4kEAGeX4DTgalt2GQUNY7oQ
+class DateTimeMultiWidget(MultiWidget):
 
-class _Date(forms.DateInput):
-    input_type = 'date'
+    def __init__(self, widgets=None, attrs=None):
+        if widgets is None:
+            if attrs is None:
+                attrs = {}
+            date_attrs = attrs.copy()
+            time_attrs = attrs.copy()
 
-    def __init__(self, **kwargs):
-        kwargs['format'] = '%Y-%m-%d'
-        super().__init__(**kwargs)
+            date_attrs['type'] = 'date'
+            time_attrs['type'] = 'time'
 
-
-class _Time(forms.DateInput):
-    input_type = 'time'
-
-    def __init__(self, **kwargs):
-        kwargs['format'] = '%H:%M'
-        super().__init__(**kwargs)
-
-
-class DateTime(forms.MultiWidget):
-    def __init__(self, attrs=None):
-        widgets = [
-            _Date(format=['%Y-%m-%d']),
-            _Time(format=['%H:%M'])
-        ]
+            widgets = [
+                TextInput(attrs=date_attrs),
+                TextInput(attrs=time_attrs),
+            ]
         super().__init__(widgets, attrs)
 
     def decompress(self, value):
         if value:
-            return [
-                value.date(),
-                value.time().replace(
-                    microsecond=0).replace(
-                    milisecond=0).replace(
-                    second=0)]
+            return [value.date(), value.strftime('%H:%M')]
         return [None, None]
+
+    def value_from_datadict(self, data, files, name):
+        date_str, time_str = super().value_from_datadict(data, files, name)
+        # DateField expects a single string that it can parse into a date.
+
+        if date_str == time_str == '':
+            return None
+
+        if time_str == '':
+            time_str = '00:00'
+
+        my_datetime = datetime.strptime(date_str + ' ' + time_str, "%Y-%m-%d %H:%M")
+        # making timezone aware
+        return make_aware(my_datetime)
 
 
 # duration field
@@ -72,7 +73,7 @@ class ExersiceForm(forms.ModelForm):
         model = Exercise
         fields = '__all__'
         widgets = {
-            'start_time': DateTime(),
-            'submission_deadline': DateTime(),
+            'start_time': DateTimeMultiWidget(),
+            'submission_deadline': DateTimeMultiWidget(),
             'work_time_duration': DurationInput()
         }
