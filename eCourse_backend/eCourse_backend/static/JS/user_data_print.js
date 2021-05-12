6442dcb7f543b2569874ecf8fe61fe15378516ae
LucRome <64947931+LucRome@users.noreply.github.com>
2021-05-12 15:51:40 +0200

    remove bootstrap from user data site

--- eCourse_backend/eCourse_backend/static/JS/user_data_print.js
+++ eCourse_backend/eCourse_backend/static/JS/user_data_print.js
@@ -6,9 +6,6 @@ function userInfoPrint()
     newWin.document.open();
     newWin.document.write('\
         <html>\
-        <head>\
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">\
-        </head>\
         <body onload="window.print()">' 
             + divToPrint.innerHTML +
         '</body></html>');
