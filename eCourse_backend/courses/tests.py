ddfb8b6f45e6541044b10ba13b49d44d0a4f1492
SWE-eCourseBot <SWE-eCourseBot@users.noreply.github.com>
2021-05-17 14:28:27 +0000

    Automated autopep8 fixes

--- eCourse_backend/courses/tests.py
+++ eCourse_backend/courses/tests.py
@@ -11,14 +11,17 @@ from .models import *
 from .views import *
 
 # Create your tests here.
+
+
 class CoursesTestCase(TestCase):
-    
+
     def setUp(self):
         call_command('loadperms', 'groups.yml')
-        self.my_admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
-        
+        self.my_admin = User.objects.create_superuser(
+            'admin', 'admin@admin.com', 'admin123')
+
     def test_courses_without_login(self):
-        self.client.logout();
+        self.client.logout()
 
         response = self.client.get('/courses/delete', follow=True)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
@@ -34,17 +37,17 @@ class CoursesTestCase(TestCase):
 
     def test_create_course_form(self):
         s = Student(
-            username= 'testilon2',
-            first_name= 'Testvorname4',
-            last_name= 'Testnachname4',
-            email= 'test4@test.com'
+            username='testilon2',
+            first_name='Testvorname4',
+            last_name='Testnachname4',
+            email='test4@test.com'
         )
         s.save()
         l = Lecturer(
-            username= 'testilon',
-            first_name= 'Testvorname3',
-            last_name= 'Testnachname3',
-            email= 'test3@test.com'
+            username='testilon',
+            first_name='Testvorname3',
+            last_name='Testnachname3',
+            email='test3@test.com'
         )
         l.save()
 
@@ -58,22 +61,21 @@ class CoursesTestCase(TestCase):
         course = CourseForm(course_form)
         self.assertTrue(course.is_valid())
         course.save()
-        
-    
+
     def test_create_course_view(self):
         self.client.force_login(self.my_admin)
         s = Student(
-            username= 'testilon2',
-            first_name= 'Testvorname4',
-            last_name= 'Testnachname4',
-            email= 'test4@test.com'
+            username='testilon2',
+            first_name='Testvorname4',
+            last_name='Testnachname4',
+            email='test4@test.com'
         )
         s.save()
         l = Lecturer(
-            username= 'testilon',
-            first_name= 'Testvorname3',
-            last_name= 'Testnachname3',
-            email= 'test3@test.com'
+            username='testilon',
+            first_name='Testvorname3',
+            last_name='Testnachname3',
+            email='test3@test.com'
         )
         l.save()
 
@@ -84,7 +86,7 @@ class CoursesTestCase(TestCase):
             'student': [s.id],
             'name': 'TestCourse'
         }
-        
+
         response = self.client.post(
             reverse('create_course'), course_form)
         self.assertEqual(response.status_code, HTTPStatus.OK)
