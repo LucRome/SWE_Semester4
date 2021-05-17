9579954f09dae8c45638bb55001874ffa18f3d19
SWE-eCourseBot <SWE-eCourseBot@users.noreply.github.com>
2021-05-17 14:51:48 +0000

    Automated autopep8 fixes

--- eCourse_backend/users/management/commands/create_courses.py
+++ eCourse_backend/users/management/commands/create_courses.py
@@ -7,31 +7,44 @@ from courses.models import Course
 
 COURSE_NUM = range(11)
 
-course_names = ['Rechnerarchitekturen-19IN', 'Compilerbau-19IN', 'Web Engineering-19IN', 'Betriebssysteme-19IN', 'SWE-19IN',
-                'Signale und Systeme-19IN', 'Statistik-19IN', 'Matlab-19IN', 'Elektrotechnik-19IN', 
-                'Physik-19IN', 'ETST-19IN']
+course_names = [
+    'Rechnerarchitekturen-19IN',
+    'Compilerbau-19IN',
+    'Web Engineering-19IN',
+    'Betriebssysteme-19IN',
+    'SWE-19IN',
+    'Signale und Systeme-19IN',
+    'Statistik-19IN',
+    'Matlab-19IN',
+    'Elektrotechnik-19IN',
+    'Physik-19IN',
+    'ETST-19IN']
+
 
 def rand_word(len):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(len))
-    
+
 
 def db_to_list(type):
-    elm = User.objects.filter(type = type)
+    elm = User.objects.filter(type=type)
     res = []
     if elm:
         for e in elm:
             res.append(e.id)
     return res
 
+
 def calc_start_date():
     start_dt = date.today().replace(day=1, month=1).toordinal()
     end_dt = date.today().toordinal()
     return date.fromordinal(random.randint(start_dt, end_dt))
 
+
 def calc_end_date():
     start_dt = date.today().toordinal()
     end_dt = date.today().replace(year=(date.today().year + 1)).toordinal()
     return date.fromordinal(random.randint(start_dt, end_dt))
+
 
 def rand_num_students(students):
     n = random.randint(1, len(students))
@@ -46,23 +59,22 @@ class Command(BaseCommand):
         stud = db_to_list(3)
         for i in COURSE_NUM:
             c = Course(
-                name = course_names[i],
-                start_date = calc_start_date(), 
-                end_date = calc_end_date()
+                name=course_names[i],
+                start_date=calc_start_date(),
+                end_date=calc_end_date()
             )
 
             try:
-                c.lecturer = Lecturer.objects.get(id = random.sample(lec, 1)[0])
+                c.lecturer = Lecturer.objects.get(id=random.sample(lec, 1)[0])
                 c.save()
                 list_student_ids = rand_num_students(stud)
                 for j in range(0, len(list_student_ids)):
-                    c.student.add(Student.objects.get(id = list_student_ids[j]))
+                    c.student.add(Student.objects.get(id=list_student_ids[j]))
                     c.save()
-            except:
+            except BaseException:
                 raise CommandError('Could not create course')
 
             self.stdout.write(
                 self.style.SUCCESS(
-                    'Successfully created testcourse with name {}'.format(c.name)
-                )
-            )
+                    'Successfully created testcourse with name {}'.format(
+                        c.name)))
