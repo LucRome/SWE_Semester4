f40e7d024e0403d4a5034d3f1f232ed8a93d148c
Deps99 <66054292+Deps99@users.noreply.github.com>
2021-05-20 13:45:29 +0200

    Add files via upload
    Added courses tests

--- eCourse_backend/courses/tests.py
+++ eCourse_backend/courses/tests.py
@@ -11,17 +11,14 @@ from .models import *
 from .views import *
 
 # Create your tests here.
-
-
 class CoursesTestCase(TestCase):
-
+    
     def setUp(self):
         call_command('loadperms', 'groups.yml')
-        self.my_admin = User.objects.create_superuser(
-            'admin', 'admin@admin.com', 'admin123')
-
+        self.my_admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        
     def test_courses_without_login(self):
-        self.client.logout()
+        self.client.logout();
 
         response = self.client.get('/courses/delete', follow=True)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
@@ -37,17 +34,17 @@ class CoursesTestCase(TestCase):
 
     def test_create_course_form(self):
         s = Student(
-            username='testilon2',
-            first_name='Testvorname4',
-            last_name='Testnachname4',
-            email='test4@test.com'
+            username= 'testilon2',
+            first_name= 'Testvorname4',
+            last_name= 'Testnachname4',
+            email= 'test4@test.com'
         )
         s.save()
         l = Lecturer(
-            username='testilon',
-            first_name='Testvorname3',
-            last_name='Testnachname3',
-            email='test3@test.com'
+            username= 'testilon',
+            first_name= 'Testvorname3',
+            last_name= 'Testnachname3',
+            email= 'test3@test.com'
         )
         l.save()
 
@@ -61,21 +58,22 @@ class CoursesTestCase(TestCase):
         course = CourseForm(course_form)
         self.assertTrue(course.is_valid())
         course.save()
-
-    def test_create_course_view(self):
+        
+    
+    def test_create_and_delete_course_view(self):
         self.client.force_login(self.my_admin)
         s = Student(
-            username='testilon2',
-            first_name='Testvorname4',
-            last_name='Testnachname4',
-            email='test4@test.com'
+            username= 'testilon2',
+            first_name= 'Testvorname4',
+            last_name= 'Testnachname4',
+            email= 'test4@test.com'
         )
         s.save()
         l = Lecturer(
-            username='testilon',
-            first_name='Testvorname3',
-            last_name='Testnachname3',
-            email='test3@test.com'
+            username= 'testilon',
+            first_name= 'Testvorname3',
+            last_name= 'Testnachname3',
+            email= 'test3@test.com'
         )
         l.save()
 
@@ -87,30 +85,40 @@ class CoursesTestCase(TestCase):
             'name': 'TestCourse'
         }
 
+        course_form2 = {
+            'lecturer': l.id,
+            'start_date': '2021-06-09',
+            'end_date': '2023-06-09',
+            'student': [s.id],
+            'name': 'ChangedCourse'
+        }
+        
+        
         response = self.client.post(
             reverse('create_course'), course_form)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         query_set = Course.objects.all()
         self.assertTrue(Course.objects.filter(name='TestCourse').exists())
+        my_id = Course.objects.get(name='TestCourse').id
 
         response = self.client.post(
-            reverse('delete_course', args=(1,)))
+            reverse ('edit_course', kwargs={'id': my_id}), course_form2)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTrue(Course.objects.filter(name='ChangedCourse').exists())
+
+
+        response = self.client.post(
+            reverse('detailed_course', args=(my_id,)))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        """
+        response = self.client.post(
+            reverse('course_overview', kwargs={'page': 1}))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        """
+        response = self.client.post(
+            reverse('delte_course', args=(my_id,)))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         query_set = Course.objects.all()
         self.assertFalse(Course.objects.filter(name='TestCourse').exists())
-    """
-    def test_delete_course_view(self):
-        self.client.force_login(self.my_admin)
-
-        query_set = Course.objects.all()
-        self.assertTrue(Course.objects.filter(name='TestCourse').exists())
-
-        response = self.client.post(
-            reverse('delete_course', args=(1,)))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        query_set = Course.objects.all()
-        self.assertFalse(Course.objects.filter(name='TestCourse').exists())
-    """
