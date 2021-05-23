1fd3c6ca90c37c452e43ca7a2176f3cfd1d5302d
SWE-eCourseBot <SWE-eCourseBot@users.noreply.github.com>
2021-05-23 22:00:30 +0000

    Automated autopep8 fixes

--- frontend_tests/run_tests_helper.py
+++ frontend_tests/run_tests_helper.py
@@ -1,4 +1,4 @@
-bashCommand = "python -m unittest discover ./frontend_tests/"
 import subprocess
+bashCommand = "python -m unittest discover ./frontend_tests/"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
